27bc4cced24354b3a97d42d2cd526ba95d1d39ce
sjk5766 <wjdrnr6662@gmail.com>
2024-01-28 15:05:03 +0900

    refactor: 코드 리팩토링 & 주석 추가

--- example.js
+++ example.js
@@ -11,38 +11,24 @@ const octokit = new Octokit({
   auth: githubToken,
 });
 
-const requestedPr = {};
-for (const repo of targetRepos) {
-  // repo에 해당하는 PR 목록을 읽어온다.
-  const prs = await octokit.request('GET /repos/{owner}/{repo}/pulls', {
-    owner,
-    repo,
-    headers: { 'X-GitHub-Api-Version': '2022-11-28' },
-  });
-
-  const requestedReviews = filterValidReview(prs);
-
-  // 나에게 할당 된 리뷰 중 Slack 메시지를 위해 필요한 데이터만 추출한다.
-  for (const review of requestedReviews) {
-    const eachReview = {
-      reviewee: review.user.login,
-      link: `<${review.html_url}|${review.title}>`,
-    };
-
-    if (requestedPr[repo]) {
-      requestedPr[repo].push(eachReview);
-    } else {
-      requestedPr[repo] = [eachReview];
-    }
-  }
+const pullRequests = await getPullRequests(octokit);
+for (const pr of pullRequests) {
+  const additionalReviewerIds = await getCommentOrRequestChangeReviewerIds(
+    octokit,
+    pr.repo,
+    pr.pullNumber,
+    pr.revieweeId
+  );
+  pr.reviewers = [...pr.reviewers, ...additionalReviewerIds];
 }
 
 let convertedMessage = '';
-for (const [repo, prs] of Object.entries(requestedPr)) {
+const convertedPullRequests = convertPullRequestsByRepository(pullRequests);
+for (const [repo, prs] of Object.entries(convertedPullRequests)) {
   convertedMessage += `:rocket: *[${repo}]*\n`;
   prs.forEach(
     (pr) =>
-      (convertedMessage += `\t- 요청자: ${pr.reviewee}, 리뷰: ${pr.link}\n`)
+      (convertedMessage += `\t- 요청자: ${pr.revieweeName}, 리뷰: ${pr.link}\n`)
   );
 }
 
@@ -57,6 +43,7 @@ await web.chat.postMessage({
   username: '리뷰 비서',
 });
 
+////////////////////////////// 함수 //////////////////////////////
 function initializeConfig() {
   return {
     githubToken: config.get('github.token'),
@@ -68,9 +55,27 @@ function initializeConfig() {
 }
 
 /**
+ * https://docs.github.com/en/rest/pulls/pulls?apiVersion=2022-11-28#list-pull-requests
+ */
+async function getPullRequests(octokit) {
+  const pullRequests = [];
+  for (const repo of targetRepos) {
+    const prs = await octokit.request('GET /repos/{owner}/{repo}/pulls', {
+      owner,
+      repo,
+      headers: { 'X-GitHub-Api-Version': '2022-11-28' },
+    });
+
+    const validPrs = convertPullRequest(filterValidReview(prs));
+    validPrs.forEach((pr) => pullRequests.push(pr));
+  }
+
+  return pullRequests;
+}
+
+/**
  * PR이 open 상태이고
  * PR이 draft가 아니며
- * PR 제목에 WIP가 안 붙어 있고 (내가 속한 개발팀 규칙으로 수정중이라 리뷰할 필요가 없는 PR은 제목에 WIP를 붙임)
  * PR에 할당된 리뷰어 목록에 내 githubId가 있다면 (나에게 리뷰가 할당되어 있다면)
  */
 function filterValidReview(prs) {
@@ -78,7 +83,68 @@ function filterValidReview(prs) {
     (pr) =>
       pr.state === 'open' &&
       pr.draft === false &&
-      !pr.title.toUpperCase().includes('WIP') &&
       pr.requested_reviewers.find((reviewer) => reviewer.id === myGithubId)
   );
+}
+
+/**
+ * 하나의 PR 마다 포함하는 데이터가 많으므로 필요한 데이터만 추출
+ * 여기서 응답 받는 reviewers는 Approve, Comment, Request Change를 하지 않은 사용자
+ */
+function convertPullRequest(prs) {
+  return prs.map((pr) => ({
+    pullNumber: pr.number,
+    repo: pr.base.repo.name,
+    revieweeId: pr.user.id,
+    revieweeName: pr.user.login,
+    link: `<${pr.html_url}|${pr.title}>`,
+    reviewers: pr.requested_reviewers.map((reviewer) => reviewer.id),
+  }));
+}
+
+/**
+ * https://docs.github.com/en/rest/pulls/reviews?apiVersion=2022-11-28#list-reviews-for-a-pull-request
+ */
+async function getCommentOrRequestChangeReviewerIds(
+  octokit,
+  repo,
+  pullNumber,
+  revieweeId
+) {
+  const reviews = await octokit.request(
+    'GET /repos/{owner}/{repo}/pulls/{pull_number}/reviews',
+    {
+      owner,
+      repo,
+      pull_number: pullNumber,
+      headers: { 'X-GitHub-Api-Version': '2022-11-28' },
+    }
+  );
+
+  const reviewerIds = reviews.data.map((review) => review.user.id);
+  const excludeReviewerIds = reviews.data
+    .filter(
+      (review) => review.user.id === revieweeId || review.state === 'APPROVED'
+    )
+    .map((review) => review.user.id);
+
+  const uniqueReviewerIds = [...new Set(reviewerIds)];
+  const uniqueExcludeReviewerIds = [...new Set(excludeReviewerIds)];
+  return uniqueReviewerIds.filter(
+    (reviewerId) => !uniqueExcludeReviewerIds.includes(reviewerId)
+  );
+}
+
+function convertPullRequestsByRepository(pullRequests) {
+  const result = {};
+  for (const pr of pullRequests) {
+    const repo = pr.repo;
+    if (result[repo]) {
+      result[repo].push(pr);
+    } else {
+      result[repo] = [pr];
+    }
+  }
+
+  return result;
 }
